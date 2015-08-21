b10a40f2d5c679976eeeb1d697c2b161b5ba1879
Robert Robinson <rerobins@gmail.com>
2015-08-21 10:48:21 -0600

    Implement basic RDF publish work
    RDF publish now works with promises in order to retrieve the data received from the first responder to the request.  This commit provides basic functionality for this service to work with little to no error handling.

--- rhobot/components/rdf_publish.py
+++ rhobot/components/rdf_publish.py
@@ -7,12 +7,10 @@ Need to figure out how to handle all of the methods associated with the function
 For the response to a message it can be easily done by executing the command in a blocking thread and then respond to
 it.
 
-The problem is how to get information when this bot is the one requesting the information.  Because I want to block
-processing of the thread that is making the request.  I really want the functionality here to block like an iq message
-does in the sleekxmpp framework.
+Promises provide the functionality for making requests and gathering results.  When selecting a single response, the
+promise will be resolved immediately after the first response is received.
 
-
-
+Promises selecting all responses will have to wait for the timeout to occur before resolving the promise.
 """
 
 from sleekxmpp.plugins.base import base_plugin
@@ -20,7 +18,8 @@ from sleekxmpp.xmlstream import ElementBase, register_stanza_plugin
 from sleekxmpp.plugins.xep_0004.stanza.form import Form
 from sleekxmpp import Message
 from rhobot.components.roster import RosterComponent
-from rdflib.namespace import FOAF
+from rhobot.components.scheduler import Promise
+from rdflib.namespace import RDF
 import logging
 import uuid
 
@@ -65,7 +64,7 @@ class RDFPublish(base_plugin):
         logger.info('Joined the registration channel')
         self.xmpp['rho_bot_roster'].add_message_received_listener(self._receive_request_message)
 
-    def send_out_request(self, payload, callback, timeout=10.0):
+    def send_out_request(self, payload, timeout=10.0):
         """
         Send out an rdf request for the provided payload.
         :param payload: the payload to serialize and then
@@ -81,12 +80,16 @@ class RDFPublish(base_plugin):
 
         thread_identifier = str(uuid.uuid4())
 
-        self._pending_requests[thread_identifier] = callback
+        promise = Promise(self.xmpp['rho_bot_scheduler'])
+
+        self._pending_requests[thread_identifier] = self._generate_single_fetch(promise, thread_identifier)
 
         self.xmpp['rho_bot_roster'].send_message(payload=rdf_stanza, thread_id=thread_identifier)
 
-        self.xmpp['rho_bot_scheduler'].schedule_event(callback=self._generate_cancel_event(callback, thread_identifier),
-                                                      delay=timeout)
+        self.xmpp['rho_bot_scheduler'].schedule_task(callback=self._generate_cancel_event(promise, thread_identifier),
+                                                     delay=timeout)
+
+        return promise
 
     def add_message_handler(self, callback):
         """
@@ -94,6 +97,7 @@ class RDFPublish(base_plugin):
         :param callback:
         :return:
         """
+        self._handlers.append(callback)
 
     def _receive_request_message(self, message):
         """
@@ -104,41 +108,53 @@ class RDFPublish(base_plugin):
         """
         logger.info('Received a request message: %s' % message)
 
-        # TODO: Check to see if there is an rdf request message in the payload.  This method will also be responsible
-        # for handling all of the incoming respones and giving them to the appropriate callbacks.
-        rdf_payload = message.get('rdf', None)
-        if rdf_payload:
-            command_type = rdf_payload.get('command', 'ignore')
-            if command_type == 'request':
-                for handler in self._handlers:
-                    response = handler(rdf_payload)
-                    if response:
-                        self.xmpp['rho_bot_roster'].send_message(payload=response,
-                                                                 thread_id=message.get('thread', None))
-                    # TODO: generate a method that will respond to the message if a payload is returned by the handler
-                    # and execute it in the scheduler.
-                    pass
-            elif command_type == 'response':
-                thread_identifier = message.get('thread', None)
-                if thread_identifier:
-                    handler = self._pending_requests.get(thread_identifier, None)
-                    if handler:
-                        handler(rdf_payload)
+        rdf_payload = message.get('rdf')
+        command_type = rdf_payload.get('command', 'ignore')
+        if command_type == 'request':
+            for handler in self._handlers:
+                response = handler(rdf_payload)
+                if response:
+                    logger.info('response')
+                    rdf_stanza = RDFStanza()
+                    rdf_stanza['command'] = 'response'
+                    rdf_stanza.append(response)
+                    self.xmpp['rho_bot_roster'].send_message(payload=rdf_stanza,
+                                                             thread_id=message.get('thread', None))
+        elif command_type == 'response':
+            thread_identifier = message.get('thread', None)
+            if thread_identifier:
+                callback = self._pending_requests.get(thread_identifier, None)
+                if callback:
+                    callback(rdf_payload)
         else:
-            logger.debug('Skipping message handling')
+            logger.info('Ignoring message')
 
-    def _generate_cancel_event(self, callback, request_identifier):
+    def _generate_cancel_event(self, promise, request_identifier):
         """
         Generate a callback that will cancel the handler and notify the callback that the handler has timed out.
-        :param callback:
+        :param promise:
         :return:
         """
         def call_back_method():
             if request_identifier in self._pending_requests:
                 del self._pending_requests[request_identifier]
 
-            callback(payload=None, timeout=True)
+                promise.resolved([])
 
         return call_back_method
+
+    def _generate_single_fetch(self, promise, thread_identifier):
+
+        def single_fetch(rdf):
+            form = rdf['form']
+            data = []
+            for record in form.get_items():
+                data.append(record[str(RDF.about)])
+
+            promise.resolved(data)
+            del self._pending_requests[thread_identifier]
+
+        return single_fetch
+
 
 rho_bot_rdf_publish = RDFPublish
