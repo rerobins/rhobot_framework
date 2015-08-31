1c8952cf6af38b3c0b3b5012ff0245a6b2188b36
Robert Robinson <rerobins@gmail.com>
2015-08-31 12:23:39 -0600

    Reorganizing the test cases.

--- test/promises/test_2_2_4.py
+++
@@ -1,7 +0,0 @@
-"""
-2.2.4: `onFulfilled` or `onRejected` must not be called until the execution context stack contains only platform code.
-https://github.com/promises-aplus/promises-tests/blob/2.1.1/lib/tests/2.2.4.js
-
-I don't know if this is testable since this implementation is currently running in a multi-threaded environment, unlike
-the javascript environment that the proimse tests and spec were developed for.
-"""
--- test/promises/test_2_3_3.py
+++
@@ -1,6 +0,0 @@
-"""
-2.3.3: Otherwise, if `x` is an object or function,
-https://github.com/promises-aplus/promises-tests/blob/2.1.1/lib/tests/2.3.3.js
-
-I don't think these apply to a python implementation.
-"""
