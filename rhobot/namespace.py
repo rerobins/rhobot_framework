0c9ec89b2b42b46ab1093700c6a78bf7e1596f82
Robert Robinson <rerobins@gmail.com>
2015-09-02 12:40:47 -0600

    Move common namespaces to rhobot.

--- rhobot/namespace.py
+++ rhobot/namespace.py
@@ -1,3 +1,15 @@
-from rdflib.namespace import ClosedNamespace
+from rdflib.namespace import ClosedNamespace, Namespace
 
 RHO = ClosedNamespace(uri='urn:rho:', terms=['Owner'])
+
+#: Namespace for working with events.
+EVENT = Namespace('http://purl.org/NET/c4dm/event.owl#')
+
+#: Namespace for working with schema.org data types
+SCHEMA = Namespace('http://schema.org/')
+
+#: Namespace for working with WGS84 data types
+WGS_84 = Namespace('http://www.w3.org/2003/01/geo/wgs84_pos#')
+
+#: Namespace for working with location data types
+LOCATION = Namespace('http://www.w3.org/ns/locn#')
