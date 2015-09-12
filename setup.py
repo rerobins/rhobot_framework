0ec8fbc0ed55a278498459e15b1bb4dfb7c18a62
Robert Robinson <rerobins@gmail.com>
2015-09-12 11:06:07 -0600

    Clean up requirements and setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,24 @@ from distutils.core import setup
 setup(
     name='rhobot',
     version='1.0.0',
-    packages=['rhobot', 'rhobot.components', 'rhobot.components.commands', 'rhobot.components.storage', ],
+    packages=[
+        'rhobot',
+        'rhobot.components',
+        'rhobot.components.commands',
+        'rhobot.components.stanzas',
+        'rhobot.components.storage',
+        'rhobot.components.storage.payload',
+    ],
     url='',
     license='BSD',
     author='Robert Robinson',
     author_email='rerobins@meerkatlabs.org',
     description='Rhobot Core',
-    install_requires=['sleekxmpp==1.3.1', 'rdflib==4.2.0', 'enum34==1.0.4']
+    install_requires=[
+        'sleekxmpp==1.4.0',
+        'rdflib==4.2.0',
+        'enum34==1.0.4'],
+    dependency_links=[
+        'http://github.com/rerobins/SleekXMPP/tarball/rhobot#egg=sleekxmpp-1.4.0',
+    ]
 )
