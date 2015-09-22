48451f17d8d9ab3607ebb098280ba84bc6ca9a73
Robert Robinson <rerobins@gmail.com>
2015-09-22 15:49:30 -0600

    FormField and FieldOption should be iterable
    That is how they are defined in the xep_0004 plugin.

--- test/components/rdf_publish/test_rdf_stanza.py
+++ test/components/rdf_publish/test_rdf_stanza.py
@@ -1,16 +1,14 @@
 """
 Unit tests for the RDF Stanza generation and manipulation.
 """
-import unittest
 from sleekxmpp.test import SleekTest
-import mock
 from sleekxmpp import Message
 from sleekxmpp.xmlstream import register_stanza_plugin
 import sleekxmpp.plugins.xep_0004 as xep_0004
 from rhobot.components.rdf_publish import RDFStanza, RDFStanzaType, RDFSourceStanza
 from rhobot.components.storage import StoragePayload
 from rhobot.namespace import RHO
-from rdflib.namespace import FOAF, RDF, RDFS
+from rdflib.namespace import FOAF, RDF
 
 
 class RDFStanzaTestCase(SleekTest):
@@ -18,8 +16,8 @@ class RDFStanzaTestCase(SleekTest):
     def setUp(self):
         register_stanza_plugin(Message, RDFStanza)
         register_stanza_plugin(RDFStanza, xep_0004.Form)
-        register_stanza_plugin(xep_0004.Form, xep_0004.FormField)
-        register_stanza_plugin(xep_0004.FormField, xep_0004.FieldOption)
+        register_stanza_plugin(xep_0004.Form, xep_0004.FormField, iterable=True)
+        register_stanza_plugin(xep_0004.FormField, xep_0004.FieldOption, iterable=True)
         register_stanza_plugin(RDFStanza, RDFSourceStanza)
         register_stanza_plugin(Message, xep_0004.Form)
 
